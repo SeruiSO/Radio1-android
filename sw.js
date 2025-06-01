93b161ec2d74c856a589e647c7bffe2548b0f812
SeruiSO <Olars.m.1@gmail.com>
2025-06-01 12:43:21 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "radio-pwa-cache-v901"; // Оновлено версію кешу
+﻿const CACHE_NAME = "radio-pwa-cache-v951"; // Оновлено версію кешу
 const urlsToCache = [
   "/",
   "index.html",
